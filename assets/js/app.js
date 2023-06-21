ba9de2613be9ab9a2c7e3fb51d407ee179e1bc95
axelolea <axelcedilloolea@gmail.com>
2023-06-20 23:47:35 -0600

    Correccion de errores

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,33 @@
+// Declarando informacion de busqueda
 const baseEndpoint = 'https://api.github.com';
+const github = 'https://github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+// Colocar noombres mas descriptivos y colocar bien los selectores
+const nameElement = document.querySelector('a.name');
+const blogElement = document.querySelector('a.blog');
+const locationElement = document.querySelector('p.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+// declarar la funcion async si se usa la sentancia await
+async function displayUser(username) {
+  nameElement.textContent = 'cargando...';
+  // Separar la url del fetch para usarla despues
+  const url = `${usersEndpoint}/${username}`
+  const response = await fetch(url);
+
+  // Validar respuesta si es ok o no
+  if(!response.ok) throw new Error(`Error en la pagina ${url}`)
+
+  // no se declaro data en la funcion
+  // crearla con los datos en json
+  const data = await response.json()
+
+  // Usar brackets para las plantillas literales ``
+  nameElement.textContent = `${data.name}`;
+  nameElement.href = `${data.html_url}`
+  // O tambien mandar solo el contenido
+  blogElement.textContent = data.blog;
+  blogElement.href = data.blog;
+  locationElement.textContent = data.location;
 }
 
 function handleError(err) {
@@ -19,4 +36,4 @@ function handleError(err) {
   n.textContent = `Algo salió mal: ${err}`
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('FelipeMG-16').catch(handleError);
